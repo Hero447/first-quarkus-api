814ed116ea05fd8c733a09bd7c9f6ddbb4545846
Hero447 <sergsoldpp@gmail.com>
2026-03-11 14:55:38 +0200

    replacing implementation of mapping with mapstruct

--- src/main/java/com/mapper/CustomerMapper.java
+++ src/main/java/com/mapper/CustomerMapper.java
@@ -3,48 +3,15 @@ package com.mapper;
 import com.dto.CustomerRequest;
 import com.dto.CustomerResponse;
 import com.proto.service.Customer;
-import jakarta.enterprise.context.ApplicationScoped;
+import org.mapstruct.Mapper;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@ApplicationScoped
-public class CustomerMapper {
-    public Customer customerRequestToCustomer(CustomerRequest customerRequest) {
-        if (customerRequest == null) {
-            return null;
-        }
+@Mapper(componentModel = "jakarta")
+public interface CustomerMapper {
+    Customer customerRequestToCustomer(CustomerRequest customerRequest);
 
-        return Customer.newBuilder()
-                .setId(customerRequest.getId())
-                .setName(customerRequest.getName())
-                .setEmail(customerRequest.getEmail())
-                .build();
-    }
+    CustomerResponse customerToCustomerResponse(Customer customer);
 
-    public CustomerResponse customerToCustomerResponse(Customer customer) {
-        if (customer == null) {
-            return null;
-        }
-
-        return CustomerResponse.builder()
-                .id(customer.getId())
-                .name(customer.getName())
-                .email(customer.getEmail())
-                .build();
-    }
-
-
-    public List<CustomerResponse> customerListToCustomerResponseList(List<Customer> customerList) {
-        if (customerList == null) {
-            return null;
-        }
-
-        List<CustomerResponse> list = new ArrayList<CustomerResponse>( customerList.size() );
-        for (Customer customer : customerList) {
-            list.add(customerToCustomerResponse(customer));
-        }
-
-        return list;
-    }
+    List<CustomerResponse> customerListToCustomerResponseList(List<Customer> customerList);
 }
--- src/main/java/com/mapper/ProductMapper.java
+++ src/main/java/com/mapper/ProductMapper.java
@@ -3,48 +3,15 @@ package com.mapper;
 import com.dto.ProductRequest;
 import com.dto.ProductResponse;
 import com.proto.service.Product;
-import jakarta.enterprise.context.ApplicationScoped;
+import org.mapstruct.Mapper;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@ApplicationScoped
-public class ProductMapper {
-    public Product productRequestToProduct(ProductRequest productRequest) {
-        if (productRequest == null) {
-            return null;
-        }
+@Mapper(componentModel = "jakarta")
+public interface ProductMapper {
+    Product productRequestToProduct(ProductRequest productRequest);
 
-        return Product.newBuilder()
-                .setId(productRequest.getId())
-                .setName(productRequest.getName())
-                .setPrice(productRequest.getPrice())
-                .build();
-    }
+    ProductResponse productToProductResponse(Product product);
 
-    public ProductResponse productToProductResponse(Product product) {
-        if (product == null) {
-            return null;
-        }
-
-        return ProductResponse.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .price(product.getPrice())
-                .build();
-    }
-
-
-    public List<ProductResponse> productListToProductResponseList(List<Product> productList) {
-        if (productList == null) {
-            return null;
-        }
-
-        List<ProductResponse> list = new ArrayList<ProductResponse>(productList.size());
-        for (Product product : productList) {
-            list.add(productToProductResponse(product));
-        }
-
-        return list;
-    }
+    List<ProductResponse> productListToProductResponseList(List<Product> productList);
 }
